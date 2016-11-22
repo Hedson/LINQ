d120d52b3919144c586d328455c5b73bf60dc66a
Kamil <bkamilb@gmail.com>
2016-11-22 21:58:32 +0100

    Many queries

--- LINQ2/Program.cs
+++ LINQ2/Program.cs
@@ -11,17 +11,30 @@ namespace LINQ2
         static void Main()
         {
 
-            // Specify the data source.
-            int[] scores = new int[] { 97, 92, 81, 60, 111 };
+            // Query #1.
+            List<int> numbers = new List<int>() { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
 
-            // Define the query expression.
-            IEnumerable<int> scoreQuery =
-                from score in scores
-                where score == 111 || score == 81
-                select score;
+            // The query variable can also be implicitly typed by using var
+            IEnumerable<int> filteringQuery =
+                from num in numbers
+                where num < 3 || num > 7
+                select num;
+
+            // Query #2.
+            IEnumerable<int> orderingQuery =
+                from num in numbers
+                where num < 3 || num > 7
+                orderby num ascending
+                select num;
+
+            // Query #3.
+            string[] groupingQuery = { "carrots", "cabbage", "broccoli", "beans", "barley" };
+            IEnumerable<IGrouping<char, string>> queryFoodGroups =
+                from item in groupingQuery
+                group item by item[0];
 
             // Executte the query.
-            foreach (int i in scoreQuery)
+            foreach (int i in orderingQuery)
             {
                 Console.Write(i + " ");
             }
