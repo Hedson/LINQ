6a18a2e05296b602c357af34a4a07387d1a8b0f0
Kamil <bkamilb@gmail.com>
2016-11-22 21:57:24 +0100

    First console program, define and execute the query

--- LINQ2/Program.cs
+++ LINQ2/Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace LINQ2
 {
-    class Program
+    class LINQQueryExpressions
     {
-        static void Main(string[] args)
+        static void Main()
         {
+
+            // Specify the data source.
+            int[] scores = new int[] { 97, 92, 81, 60, 111 };
+
+            // Define the query expression.
+            IEnumerable<int> scoreQuery =
+                from score in scores
+                where score == 111 || score == 81
+                select score;
+
+            // Executte the query.
+            foreach (int i in scoreQuery)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.ReadKey();
         }
     }
 }
