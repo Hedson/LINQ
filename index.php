6a35ce0dbf5b96a5f7ae4a86bbd704b889d63e45
Kamil Bara <bkamilb@gmail.com>
2016-12-16 22:08:04 +0100

    Created a homepage for the website
    description yeyee

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+
+<head>
+	<meta charset="utf-8" />
+	<title>Najlepsze filmy!</title>
+	<meta name="description" content = "Przed To¹ spis moich ulubionych filmów, sprawdŸ, czy znasz
+	je wszystke. Uczta dla kinomaniaka" />
+	<meta name="keywords" content="film,kino,najlepsze filmy, top filmy" />
+	<meta http-equiv="X-UA-Compatible" content ="IE=edge,chrome=1" />
+	
+</head>
+
+<body>
+ 
+ <h1>Najlepsze filmy!</h1>
+ 
+ Komedia: <br /> 
+ 
+ <a href="http://www.filmweb.pl/Forrest.Gump" target="_blank" title="zobacz na Filmweb-ie!">
+ Forrest Gump(1994)
+ </a>
+ 
+ <br /><br />
+
+ <a href="http://www.filmweb.pl/Forrest.Gump" target="_blank" title="Run Forest, RUN!"><img src="img/forrest.jpg" />
+ </a>
+ 
+ <br /><br />  <br /><br />
+Horror: <br /> 
+
+ 
+ <a href="http://www.filmweb.pl/Obcy" target="_blank" title="Zobacz na Filmwebie!">
+ Obcy 8 pasa¿er Nostromo (1979)
+ </a>
+ 
+   <br /><br />
+   
+ <a href = "http://www.filmweb.pl/Obcy" target="_blank" title="one ugly basterd!"><img src="img/alien.jpg" />
+ </a>
+ 
+ 
+ 
+ 
+ 
+</body>
+
+
+
+
+</html>
